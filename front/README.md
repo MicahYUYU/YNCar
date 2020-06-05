bab3bc95ad13b85ac4d2fc53db91b16c41457edd
MicahYUYU <x2646609036@163.com>
2020-06-05 16:54:31 +0800

    init project config

---
+++ front/README.md
@@ -0,0 +1,40 @@
+# 五组一诺汽车前端仓库
+
+### 工具
+
+- 包管理器 => npm
+- CLI => vue-cli4
+
+### 开发者
+
+- 陈迎语
+
+- 夏铭
+
+- 杨璞
+
+- Micah 陈旭辉
+
+### 安装依赖
+
+```
+npm install
+```
+
+### 启动 devServer
+
+```
+npm run serve
+```
+
+### 启动 Vue UI
+
+```
+vue ui
+```
+
+### lint 检查
+
+```
+npm run lint
+```
---
+++ front/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  -khtml-user-select: none;
+  user-select: none;
+}
+
+/* #nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+
+html,
+body,
+#app {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>

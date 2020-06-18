76de9b1817b776244de60bc346b6ffb78ff712b0
Pizifly <imaginedreampzfly@gmail.com>
2020-06-18 14:30:13 +0800

    re-edit about

--- front/src/views/About/index.vue
+++ front/src/views/About/index.vue
@@ -38,42 +38,41 @@
           <span class="blueWord">专业、公正、客观</span>的第三方检测服务。
         </p>
       </div>
+      <p style="font-size: 20px; font-weight: bold; line-height: 40px;">秉承“致力车辆买卖信息对称，健全车辆流通信用体制”的使命</p>
+      <p style="color: #b6b6b6;">一诺检测建立起一支由汽车研发工程师、整车评审工程师以及资深维修技师组成的专业检测师团队</p>
+      <p style="color: #b6b6b6;">只为让更多的客户可以享受到车辆买卖信息对称带来的优质体验</p>
     </div>
-    <div class="info">
-      <div class="main_info">
-        <p style="font-size: 20px; font-weight: bold; line-height: 40px;">秉承“致力车辆买卖信息对称，健全车辆流通信用体制”的使命</p>
-        <p style="color: #b6b6b6;">一诺检测建立起一支由汽车研发工程师、整车评审工程师以及资深维修技师组成的专业检测师团队</p>
-        <p style="color: #b6b6b6;">只为让更多的客户可以享受到车辆买卖信息对称带来的优质体验</p>
-        <div class="img2">
-          <img src="@/assets/img_about/img2.jpg" class="zoom" />
-        </div>
-        <div class="intro1">
-          <img src="@/assets/img_about/intro1.png" />
-        </div>
-        <div class="img3">
-          <img src="@/assets/img_about/img3.jpg" class="zoom" />
-        </div>
-        <div class="intro2">
-          <img src="@/assets/img_about/intro2.png" />
-        </div>
-        <div class="img4">
-          <img src="@/assets/img_about/img4.jpg" class="zoom" />
-        </div>
-        <div class="intro3">
-          <img src="@/assets/img_about/intro3.png" />
-        </div>
-        <div class="bg">
-          <img src="@/assets/img_about/bg.jpg" />
-        </div>
-        <p class="bg_title">坚持“专注细节、品质服务”的理念</p>
-        <hr class="line_2" />
-        <div class="bg_word">
-          <p class="w1">一诺检测是隶属于伤害振森汽车服务有限公司的一家第三方车辆检测平台，</p>
-          <p class="w2">并以《二手车鉴定评估技术规范》的国家标准为基础，</p>
-          <p class="w3">综合车辆流通市场实际需求，</p>
-          <p class="w4">研发并制定出涵盖检测项目：229项的专业检测车辆鉴定评估体系，</p>
-          <p class="w5">力求每一个检测细节都做到极致。</p>
-        </div>
+
+    <div class="main_info">
+      <div class="img2">
+        <img src="@/assets/img_about/img2.jpg" class="zoom" />
+      </div>
+      <div class="intro1">
+        <img src="@/assets/img_about/intro1.png" />
+      </div>
+      <div class="img3">
+        <img src="@/assets/img_about/img3.jpg" class="zoom" />
+      </div>
+      <div class="intro2">
+        <img src="@/assets/img_about/intro2.png" />
+      </div>
+      <div class="img4">
+        <img src="@/assets/img_about/img4.jpg" class="zoom" />
+      </div>
+      <div class="intro3">
+        <img src="@/assets/img_about/intro3.png" />
+      </div>
+      <div class="bg">
+        <img src="@/assets/img_about/bg.jpg" />
+      </div>
+      <p class="bg_title">坚持“专注细节、品质服务”的理念</p>
+      <hr class="line_2" />
+      <div class="bg_word">
+        <p class="w1">一诺检测是隶属于伤害振森汽车服务有限公司的一家第三方车辆检测平台，</p>
+        <p class="w2">并以《二手车鉴定评估技术规范》的国家标准为基础，</p>
+        <p class="w3">综合车辆流通市场实际需求，</p>
+        <p class="w4">研发并制定出涵盖检测项目：229项的专业检测车辆鉴定评估体系，</p>
+        <p class="w5">力求每一个检测细节都做到极致。</p>
       </div>
     </div>
     <div>
@@ -86,7 +85,6 @@
         <span class="iwr">陪同看车</span>
       </div>
     </div>
-
     <div style="height: 200px; overflow: hidden;">
       <p style="font-size: 20px; font-weight: bold; line-height: 40px;">坚信“用户满意度是企业生存的基石”</p>
       <p style="color: #b6b6b6;">一诺检测率先将移动互联网技术运用到第三方检测中，用户可以通过微信、html5、电话、网站轻松享用检测服务。</p>
@@ -120,22 +118,22 @@ export default {
   components: {
     //这里是用到等各种组件
     YHeader,
-    YFooter,
+    YFooter
   },
-  data () {
+  data() {
     return {
       //这里返回变量
     };
   },
   methods: {
-    moveL () {
+    moveL() {
       this.$("#picL").animate({ left: 0 }, 500);
     },
-    moveR () {
+    moveR() {
       this.$("#picR").animate({ right: "100%" }, 500);
     }
   },
-  mounted () {
+  mounted() {
     //这里是页面加载完毕以后要自动执行的函数
     this.moveL();
     this.moveR();
@@ -255,6 +253,7 @@ export default {
 
   .info {
     position: relative;
+    display: flex;
     width: 100%;
     overflow: hidden;
   }
@@ -265,8 +264,10 @@ export default {
 
   .main_info {
     position: relative;
-    height: 800px;
-    width: 1560px;
+    margin: 0, auto;
+    left: 0;
+    height: 1030px;
+    width: 1190px;
     overflow: hidden;
   }
 
@@ -274,7 +275,7 @@ export default {
     height: 300px;
     width: 300px;
     position: relative;
-    left: 355px;
+    left: 0px;
     overflow: hidden;
   }
 
@@ -283,7 +284,7 @@ export default {
     width: 312px;
     position: relative;
     top: -300px;
-    left: 643px;
+    left: 288px;
   }
 
   .img3 {
@@ -291,7 +292,7 @@ export default {
     width: 300px;
     position: relative;
     top: -600px;
-    left: 955px;
+    left: 600px;
     overflow: hidden;
   }
 
@@ -300,7 +301,7 @@ export default {
     width: 312px;
     position: relative;
     top: -900px;
-    left: 1243px;
+    left: 888px;
   }
 
   .intro3 {
@@ -308,7 +309,8 @@ export default {
     width: 312px;
     position: relative;
     top: -1200px;
-    left: 355px;
+    left: 0px;
+    z-index: 4;
   }
 
   .img4 {
@@ -316,27 +318,29 @@ export default {
     width: 300px;
     position: relative;
     top: -900px;
-    left: 655px;
+    left: 300px;
     overflow: hidden;
+    z-index: 3;
   }
   .bg {
     height: 300px;
     width: 300px;
     position: relative;
     top: -1500px;
-    left: 955px;
+    left: 600px;
+    z-index: -1;
   }
   .bg_title {
     position: relative;
     color: white;
     font-weight: bold;
     top: -1790px;
-    left: 420px;
+    left: 240px;
   }
   .line_2 {
     position: relative;
     top: -1785px;
-    left: 324px;
+    left: 145px;
     border: solid 0.3px #dddddd;
     width: 47px;
     height: 0.1px;
@@ -347,8 +351,8 @@ export default {
     height: 300px;
     position: relative;
     top: -1780px;
-    left: 700px;
-    z-index: 3;
+    left: 335px;
+    z-index: 0;
     .w1 {
       position: relative;
       font-weight: 20;

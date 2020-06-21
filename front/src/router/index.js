7e382271131e94d410a96b9904cd695e00166dca
CYYukio <1365164156@qq.com>
2020-06-21 23:32:11 +0800

    fix: form unfinished

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -33,27 +33,6 @@ const routes = [
         name: 'enterprise',
         component: Enterprise
     }
-    // {
-    //   path: '/about',
-    //   name: 'About',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
-    // {
-    //     path: '/',
-    //     name: 'layout',
-    //     component: Layout,
-    //     redirect: '/home',
-    //     children: [
-    //         {
-    //             path: '/home',
-    //             name: '首页',
-    //             component: Home
-    //         }
-    //     ]
-    // }
 ]
 
 const router = new VueRouter({
